f2df08a4cabab88fc37bb3305b7007904750115a
fernando.groeler <fernando.groeler@gmail.com>
2018-02-27 09:21:52 -0300

    correção module exports - database

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/mymoney')
 
 const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/mymoney'
 module.exports = mongoose.connect(url, { useMongoClient: true })
