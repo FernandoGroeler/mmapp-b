ed94f01add64dabb91ed8fe836c31f3993a35892
fernando.groeler <fernando.groeler@gmail.com>
2018-02-27 19:26:52 -0300

    Mudando variavel de ambiente.

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/mymoney'
+const url = process.env.MONGOLAB_URI ? process.env.MONGODB_URI : 'mongodb://localhost/mymoney'
 module.exports = mongoose.connect(url, { useMongoClient: true })
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
