14671b52de268a8acaa4df7e0291a0f0bede4f44
fernando.groeler <fernando.groeler@gmail.com>
2018-02-28 07:58:57 -0300

    Ajustado connect mongoose.

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/mymoney'
-module.exports = mongoose.connect(url, { useMongoClient: true })
+module.exports = mongoose.connect(url, {})
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
